7afccbfca63ded7fe9f21b86fb7d40fdc463a8fc
Teages <Teages@teages.xyz>
2025-11-19 22:43:55 +0800

    chore: init project

---
+++ src/main/kotlin/dev/teages/paperPluginEasyShulker/PaperPluginEasyShulker.kt
@@ -0,0 +1,14 @@
+package dev.teages.paperPluginEasyShulker
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PaperPluginEasyShulker : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
